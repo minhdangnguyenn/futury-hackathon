b6c8d8aaa7f437276de377da89a314f13796bf23
minhdangnguyenn <dangminh214@gmail.com>
2026-04-23 20:44:14 +0200

    change icons

--- freiessen/src/app/(frontend)/layout.tsx
+++ freiessen/src/app/(frontend)/layout.tsx
@@ -4,6 +4,11 @@ import './styles.css'
 export const metadata = {
   description: 'Viega Dashboard.',
   title: 'Viega Dashboard',
+  icons: {
+    icon: '/favicon.svg',
+    shortcut: '/favicon.svg',
+    apple: '/favicon.svg',
+  },
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
